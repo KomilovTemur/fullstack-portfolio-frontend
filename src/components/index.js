abf72ad07fba824a16eea41f1dc4c1fd8bdd5653
Temur Komilov <itteamdev74@gmail.com>
2022-09-21 09:57:18 +0500

    all components imported here

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Services from "./services/Services";
+import Portfolio from "./portfolio/Portfolio";
+import Footer from "./footer/Footer";
+import React from "react";
+const Imports = () => {
+  return (
+    <>
+      <Header />
+      <Services />
+      <Portfolio />
+      <Footer />
+    </>
+  );
+};
+
+export default Imports;
